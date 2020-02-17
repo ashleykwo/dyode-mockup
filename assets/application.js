52c7e29d74798b497f87dd711bfa547053e3542b
Ashley <ashley.kwo@gmail.com>
2020-02-16 22:50:02 -0600

    Created footer and image with text overlay section

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,22 @@
-// Put your applicaiton javascript here
+/* ACCORDION */
+// Get tab and content classes in list
+var accordion = document.getElementsByClassName('accordion-heading');
+var content = document.getElementsByClassName('content');
+
+// Iterate through tabs class list
+for (var i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener('click', function() {
+    // when tabs list doesn't contain any active class, add active to clicked tab and show its content
+    if (!this.classList.contains('active')) {
+      this.classList.add('active');
+      var currentTab = this.getAttribute("data-tab");
+      var element = document.getElementById(currentTab);
+      element.classList.add('show');
+    } else {
+      this.classList.remove('active');
+      var currentTab = this.getAttribute("data-tab");
+      var element = document.getElementById(currentTab);
+      element.classList.remove('show');
+    }
+  });
+}
